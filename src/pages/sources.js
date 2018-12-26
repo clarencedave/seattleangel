79a20a16253e9e4850b1fa57a7f9f486f1f78706
Cla2 <clarencedave990@gmail.com>
2018-12-26 08:55:17 +0800

    about + resource 2

---
+++ src/pages/sources.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+
+
+const ResourcesPage = () => (
+  <Layout>
+    <br/> 
+      <br/>
+      <h1>Resources</h1>
+
+      <p>Our newsletter (sign up here) is the best place to stay up to date with the latest happenings around Seattle for angel investors and entrepreneurs looking to raise capital. There are lots of other great resources around Seattle such as Startup Seattle (yes even the local government is interested in seeing startups succeed). Here are some other great resources in Seattle:</p>
+
+      <h3>Accelerator Programs</h3>
+     <a href = "http://fledge.co/"> <li>Fledge</li> </a>
+     <a href = "https://fi.co/information/mentors?target=seattle"> <li>Founders Institute</li> </a>
+     <a href = "https://www.9milelabs.com/"> <li>9mile labs</li> </a>
+     <a href = "Techstars Seattle"> <li>Techstars Seattle</li> </a>
+      <h3>Pre-sales crowdfunding for products</h3>
+     <a href = "https://www.kickstarter.com/"> <li>www.kickstarter.com</li> </a>
+     <a href = "www.indiegogo.com"> <li>www.indiegogo.com</li> </a>
+      <h3>Angel/Venture Funding through online exchanges</h3>
+     <a href = "https://www.seedinvest.com/"> <li>SeedInvest</li> </a>
+     <a href = "https://angel.co/"> <li>www.angel.co</li> </a>
+     <a href = "https://www.dreamfunded.com/"> <li>DreamFunded</li> </a>
+     <a href = "https://microventures.com/"> <li>MicroVentures</li> </a>
+     <a href = "https://www.startups.co/"> <li>www.startups.co</li> </a>
+     <a href = "http://yescorvallis.org/start-up/funding/www.circleup.com"> <li>www.circleup.com</li> </a>
+     <a href = "https://fundersclub.com/"> <li>www.fundersclub.com</li> </a>
+     <a href = "http://yescorvallis.org/start-up/funding/www.equitynet.com"> <li>www.equitynet.com</li> </a>
+      <h3>Traditional Angel Groups</h3>
+     <a href = "https://www.pugetsoundvc.com/"> <li>Puget Sound Venture Club </li> </a>
+     <a href = "https://www.allianceofangels.com/"> <li>Alliance Of Angels</li> </a>
+     <a href = "http://www.bellinghamangels.com/go/site/1064/"> <li>Bellingham Angels</li> </a>
+     <a href = "http://www.keiretsuforum.com/frontend/ManageChapterSection.aspx?Page=Welcome&ChapterId=14"> <li>Keiretsu forum</li> </a>
+     <a href = "http://www.seattleangelconference.com/"> <li>Seattle Angel Conference</li> </a>
+     <a href = "https://seachange.fund/"> <li>Seattle Angel Fund</li> </a>
+     <a href = "https://www.tacomaangelnetwork.com/home"> <li>Tacoma Angel Network</li> </a>
+     <a href = "https://www.e8angels.com/"> <li>Element8</li> </a>
+     <a href = "http://tieangelsseattle.org/"> <li>TAGS</li> </a>
+     <a href = "http://medtechwings.com/"> <li>WINGS</li> </a>
+     <a href = "http://vantec.ca/cpages/homepage"> <li>Vantec</li> </a>
+      <h3>Traditional Venture Capital</h3>
+     <a href = "https://www.archventure.com/"> <li>Arch Venture</li> </a>
+     <a href = "http://www.benaroya.com/error/notfound?aspxerrorpath=/venturecapital"> <li>Benaroya Venture Capital</li> </a>
+     <a href = "http://www.bezosexpeditions.com/"> <li>Bezos Expeditions</li> </a>
+     <a href = "http://www.cedargroveinv.com/"> <li>Cedar Grove Investments</li> </a>
+     <a href = "http://www.divergent.com/"> <li>Divergent Ventures</li> </a>
+     <a href = "http://www.flukeventures.com/"> <li>Fluke Ventures</li> </a>
+     <a href = "http://www.founderscoop.com/"> <li>Founders Co-op</li> </a>
+     <a href = "http://www.guideventures.com/"> <li>Guide Ventures</li> </a>
+     <a href = "https://www.ignitionpartners.com/"> <li>Ignition Partners</li> </a>
+     <a href = "https://www.lightercapital.com/"> <li>Lighter Capital</li> </a>
+     <a href = "http://www.madrona.com/"> <li>Madrona</li> </a>
+     <a href = "https://www.maveron.com/"> <li>Maveron</li> </a>
+     <a href = "https://www.newmediaventures.org/"> <li>New Media Ventures</li> </a>
+     <a href = "http://nwva.com/?reqp=1&reqr=nzcdYzMlozqarKWhLKElrF5jLab="> <li>NW Venture Associates</li> </a>
+     <a href = "http://www.ovp.com/"> <li>OVP</li> </a>
+     <a href = "https://www.polarispartners.com/"> <li>Polaris Ventures</li> </a>
+     <a href = "http://www.seapointventures.com/home.cfm"> <li>SeaPoint Ventures</li> </a>
+     <a href = "http://www.secondave.com/"> <li>Second Avenue Partners</li> </a>
+     <a href = "https://venturemechanics.com/"> <li>Venture Mechanics</li> </a>
+     <a href = "http://www.voyagercapital.com/"> <li>Voyager Capital</li> </a>
+     <a href = "https://capital.vulcan.com/"> <li>Vulcan Capital</li> </a>
+     <a href = "http://www.wrfseattle.org/capital/index.shtml"> <li>WRF Capital </li> </a>
+      <h3>Supporting organizations</h3>
+     <a href = " "> <li>Eastside Incubator</li> </a>
+     <a href = "http://eoseattle.org/"> <li>Entrepreneurs Organization</li> </a>
+     <a href = "http://founderdating.com/upcoming/seattle/"> <li>Founder Dating</li> </a>
+     <a href = "https://www.geekwire.com/"> <li>GeekWire</li> </a>
+     <a href = "http://www.mitwa.org/"> <li>MIT Forum Northwest</li> </a>
+     <a href = "http://pmconsortium.ning.com/"> <li>Product Management Consportium</li> </a>
+     <a href = "http://www.seattletechcalendar.com/"> <li>Seattle Tech Calendar</li> </a>
+     <a href = "http://www.seattletechwiki.com/index.php?title=Home"> <li>Seattle TechWiki</li> </a>
+     <a href = "http://seattle.tie.org/"> <li>Seattle TIE</li> </a>
+     <a href = "https://www.startupdigest.com/"> <li>Startup Digest</li> </a>
+     <a href = "https://foster.uw.edu/"> <li>UW CIE</li> </a>
+     <a href = "https://www.washingtontechnology.org/"> <li>Washington Technology Industry Association</li> </a>
+     <a href = "http://www.westsoundtechnology.org/"> <li>West Sound Technology</li> </a>
+     <a href = "https://xconomy.com/seattle/"> <li>xconomy</li> </a>
+     <a href = "https://www.score.org/"> <li>SCORE</li> </a>
+     <a href = "https://www.meetup.com/NewTechSeattle/"> <li>New Tech Seattle/Eastside</li> </a>
+     <a href = "https://www.funding-quest.com/"> <li>www.funding-quest.com</li> </a>
+     <a href = "https://www.meetup.com/seattle-startups-open-coffee/"> <li>Seattle Open Coffee</li> </a>
+     <a href = "http://www.bellevueentrepreneur.com/"> <li>Bellevue Entrepreneur Alliance</li> </a>
+     <a href = "https://www.meetup.com/Bellevue-Seattle-Women-Entrepreneurs/"> <li>Bellevue-Seattle Women Entrepreneurs</li> </a>
+     <a href = "https://seattlepoker20.wordpress.com/"> <li>Poker 2.0</li> </a>
+     <a href = "https://www.meetup.com/entrepreneurs/"> <li>Seattle Entrepreneurs</li> </a>
+     <a href = "http://smcseattle.com/"> <li>Seattle Social Media Club</li> </a>
+     <a href = "http://seattle.startupdrinks.com/"> <li>Seattle Startup Drinks</li> </a>
+     <a href = "http://www.socialgamesseattle.com/"> <li>Social Games Seattle</li> </a>
+     <a href = "https://www.meetup.com/topics/entrepreneurship/1127"> <li>Tacoma Entrepreneurs</li> </a>
+     <a href = "http://growconf.com/"> <li>GROWConf</li> </a>
+     <a href = "https://igniteseattle.com/"> <li>Ignite Seattle</li> </a>
+     <a href = "http://refreshseattle.org/"> <li>Refresh Seattle</li> </a>
+      <h3>Investment Banks</h3>
+     <a href = "http://www.ahvp.com/"> <li>Alexander Hutton</li> </a>
+     <a href = "http://www.cascadiacapital.com/"> <li>Cascadia Capital</li> </a>
+     <a href = "http://www.montlakecapital.com/"> <li>Montlake Capital</li> </a>
+     <a href = "http://paladinpartners.com/"> <li>Paladin Partners</li> </a>
+     <a href = "http://trilogy-capital.com/"> <li>Trilogy Capital Partners</li> </a>
+     <a href = "http://www.vaultcapital.com/"> <li>Vault Capital</li> </a>
+     <a href = "https://www.kauffman.org/research-and-policy/returns-of-angels-investors-in-groups"> <li>Returns to Angel Investors in Groups</li> </a><p>by Rob Wiltbank</p>
+     <a href = "http://talkfast.org/2010/04/27/angel-investing-simulation"> <li>Angel Investing Simulation</li> </a><p>by Jeff Miller</p>
+     
+
+      
+
+
+      
+
+
+  </Layout>
+)
+
+export default ResourcesPage
